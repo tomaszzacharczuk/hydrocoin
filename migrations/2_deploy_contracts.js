1586a3b96ffb6532a39183719689c5fffc2698ca
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2017-12-16 15:52:56 -0500

    migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,31 +3,24 @@ var HYCCrowdsaleICO = artifacts.require("./HYCCrowdsaleICO.sol");
 var Hydrocoin = artifacts.require("./Hydrocoin.sol");
 
 
-// An async function
-const setOwners = async function setOwners(HydrocoinInstance) {
-    var H1 = await HYCCrowdsalePreICO.deployed();
-    var H2 = await HYCCrowdsaleICO.deployed();
-    await HydrocoinInstance.addOwner(H1.address);
-    await HydrocoinInstance.transferOwnership(H2.address);
-};
-
 module.exports = function(deployer, network, accounts) {
     if (network == "develop" || network == "development") {
-        var _startTime = Math.floor(Date.now() / 1000) + 5;
-        var _endTime = Math.floor(Date.now() / 1000) + 3600;
-        var _wallet = accounts[1];
-
-        // pre ICO
-        deployer.deploy(HYCCrowdsalePreICO, _startTime, _endTime, _rate, _wallet, _hardCap);
-
-        // ICO
-        deployer.deploy(HYCCrowdsaleICO, _startTime, _endTime, _rate, _wallet, _hardCap);
-            
-        // token
-        deployer.deploy(Hydrocoin).then((instance) => await setOwners());
-
+        var _preStartTime = Math.floor(Date.now() / 1000);
+        var _startTime = Math.floor(Date.now() / 1000) + 3600;
+        var _perEndTime = Math.floor(Date.now() / 1000) + 3600;
+        var _endTime = Math.floor(Date.now() / 1000) + 3600*2;
+        var _preRate = 1100;
+        var _rate = 1000;
+        var _preWallet = accounts[1];
+        var _wallet = accounts[2];
+        var _preHardCap = web3.toWei(49900000, "ether");
+        var _hardCap = web3.toWei(450000000, "ether");
     } else if (network == "rinkeby") {
     } else if (network == "live") {
     }
 
+    // pre ICO
+    deployer.deploy(HYCCrowdsalePreICO, _preStartTime, _perEndTime, _preRate, _preWallet, _preHardCap);
+    // ICO
+    deployer.deploy(HYCCrowdsaleICO, _startTime, _endTime, _rate, _wallet, _hardCap);
 };
---
+++ migrations/3_contracts_config.js
@@ -0,0 +1,32 @@
+var HYCCrowdsalePreICO = artifacts.require("./HYCCrowdsalePreICO.sol");
+var HYCCrowdsaleICO = artifacts.require("./HYCCrowdsaleICO.sol");
+var Hydrocoin = artifacts.require("./Hydrocoin.sol");
+var Payment = artifacts.require("./Payment.sol");
+
+
+module.exports = function(deployer, network, accounts) {
+    let hyc, pay;
+    deployer.then(() => {
+        return deployer.deploy(Payment, HYCCrowdsalePreICO.address);
+    }).then((result) => {
+        return Payment.deployed();
+    }).then((instance) => {
+        pay = instance;
+        return deployer.deploy(Hydrocoin, pay.address);
+    }).then((result) => {
+        return Hydrocoin.deployed();
+    }).then((instance) => {
+        hyc = instance;
+        return hyc.addOwner(HYCCrowdsalePreICO.address);
+    }).then((result) => {
+        return hyc.addOwner(HYCCrowdsaleICO.address);
+    }).then((result) => {
+        return HYCCrowdsalePreICO.deployed();
+    }).then((instance) => {
+        return instance.assignTokenContract(hyc.address);
+    }).then((result) => {
+        return pay.setRate(1500);
+    }).then((result) => {
+        return pay.setToken(hyc.address);
+    });
+};
